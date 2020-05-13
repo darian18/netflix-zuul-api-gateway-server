a49bb5fd0da507d95ec9190537c5c1555e04f1ff
Dariusz Chmieła <dariusz.chmiela@ipfdigital.com>
2020-05-13 15:06:34 +0200

    MSA: Set gateway base properties

--- src/test/java/dch/com/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
+++ src/test/java/dch/com/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -2,7 +2,11 @@ package dch.com.netflixzuulapigatewayserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootTest
 class NetflixZuulApiGatewayServerApplicationTests {
 
